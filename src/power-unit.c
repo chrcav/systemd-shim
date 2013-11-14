b128edbb1307a7e84cff15a924bbb96ff104d441
Martin Pitt <martin.pitt@ubuntu.com>
2013-11-14 18:44:47 +0100

    5-0ubuntu1 (patches unapplied)
    Imported using git-ubuntu import.

--- src/power-unit.c
+++ src/power-unit.c
@@ -20,6 +20,7 @@
 #include "unit.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -55,7 +56,23 @@ power_unit_start (Unit *unit)
    */
   if (pu->action == POWER_OFF || pu->action == POWER_REBOOT)
     {
+      GError *error = NULL;
+      gchar *pid_str;
+      gboolean success;
+
       in_shutdown = TRUE;
+
+      /* avoid being killed during shutdown, so that we can keep our
+       * in_shutdown state */
+      pid_str = g_strdup_printf ("%u", (unsigned) getpid ());
+      success = g_file_set_contents ("/run/sendsigs.omit.d/systemd-shim.pid", pid_str, -1, &error);
+      g_free (pid_str);
+      if (!success)
+        {
+          g_warning ("Unable to write sendsigs.omit.d pid file: %s", error->message);
+          g_error_free (error);
+        }
+
       if (system (power_cmds[pu->action]) != 0)
         g_warning ("Error while running '%s'", power_cmds[pu->action]);
     }
